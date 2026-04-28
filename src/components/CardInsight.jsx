a9fa4fb598e8ca9fdfa3d32bd56ef6ad296084fc
nalugao <nalugao@hotmail.com>
2026-04-28 08:39:28 -0300

    feat: update componentes CardInsight e Insights  com novo props e estilo

--- src/components/CardInsight.jsx
+++ src/components/CardInsight.jsx
@@ -4,7 +4,6 @@ export default function CardInsight(props) {
 
     return(
         <div className="card_insight">
-            {/* <img src={props.icone} alt={props.alt} /> */}
             <p className='texto_titulo'>{props.titulo}</p>
             <p className='texto_descricao'>{props.descricao}</p>
         </div>
--- src/sections/simulador/Simulador.jsx
+++ src/sections/simulador/Simulador.jsx
@@ -5,11 +5,12 @@ import Navbar from "../../components/Navbar";
 export default function Simulador() {
     const simulatorData = {
         titulo: "Simulador de Risco",
-        subtitulo: "Estime seu perfil de risco com base na idade, frequência de uso e duração"
+        subtitulo: "Estime seu perfil de risco com base na idade, frequência de uso e duração",
+        variante: "escuto"
     }
     return(
         <section id="simulador" className="simulador_page">
-            <TituloSubtitulo titulo={simulatorData.titulo} subtitulo={simulatorData.subtitulo} />
+            <TituloSubtitulo titulo={simulatorData.titulo} subtitulo={simulatorData.subtitulo} variante={simulatorData.variante}/>
             {/* INSERIR O SIMULADOR AQUI */}
         </section>
     )
